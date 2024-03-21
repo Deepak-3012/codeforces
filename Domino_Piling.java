75815e20d3036e81893258132b86ee16fbc05db8
Deepak S <deepak.20021230@gmail.com>
2024-03-21 20:55:33 +0530

    today's program added

---
+++ Domino_Piling.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Domino_Piling {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+        int res;
+        res = (l*b)/2;
+        System.out.println(res);
+    }
+}
